224635c586a5c381e8cf830ec10f93f233cef2da
kksh3ll <kksh3ll@gmail.com>
2019-04-09 22:50:38 +0800

    create controller for test scope

---
+++ SpringIoC/webscope/src/main/java/top/jhana/ApplicationController.java
@@ -0,0 +1,15 @@
+package top.jhana;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApplicationController {
+
+    @RequestMapping("testApplication")
+    @ResponseBody
+    public String test() {
+        return this.toString();
+    }
+}
