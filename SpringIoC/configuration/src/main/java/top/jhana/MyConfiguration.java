7f8c40fa0a00ed0157e44915613cf1305a2066d3
kksh3ll <kksh3ll@gmail.com>
2019-04-13 23:15:07 +0800

    test for annotation code

--- SpringIoC/configuration/src/main/java/top/jhana/MyConfiguration.java
+++ SpringIoC/configuration/src/main/java/top/jhana/MyConfiguration.java
@@ -1,8 +1,10 @@
 package top.jhana;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(value = "top.jhana")
 public class MyConfiguration {
 
 }
--- SpringIoC/configuration/src/test/java/ConfigurationTest.java
+++ SpringIoC/configuration/src/test/java/ConfigurationTest.java
@@ -1,7 +1,18 @@
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import top.jhana.Bean2;
+import top.jhana.MyConfiguration;
+
 
 public class ConfigurationTest {
-    ApplicationContext ac = new AnnotationConfigApplicationContext("");
-    ac.
+
+    @Test
+    public void test() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(MyConfiguration.class);
+        Bean2 bean2 = ac.getBean("bean2", Bean2.class);
+        System.out.println("bean2 = " + bean2);
+    }
+
 }
