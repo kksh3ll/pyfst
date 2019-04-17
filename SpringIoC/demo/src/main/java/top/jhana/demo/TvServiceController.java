d3de0ca216bab855362e6a28766f1a40b452fd9a
kksh3ll <kksh3ll@gmail.com>
2019-04-17 22:18:10 +0800

    springboot method getAll()

--- SpringIoC/demo/src/main/java/top/jhana/demo/TvServiceController.java
+++ SpringIoC/demo/src/main/java/top/jhana/demo/TvServiceController.java
@@ -1,23 +1,49 @@
 package top.jhana.demo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/tvservice")
 public class TvServiceController {
 
+    private static final Log log = LogFactory.getLog(TvServiceController.class);
+
+//    @GetMapping
+//    public Map sayHello() {
+//
+//        Map<String, String> map = new HashMap<>();
+//        map.put("message", "hello world");
+//        return map;
+//    }
+
     @GetMapping
-    public Map sayHello() {
+    public List<TvServiceDto> getAll() {
 
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "hello world");
-        return map;
+        if (log.isTraceEnabled()) {
+            log.trace("getAll()被调用了！");
+        }
+        List<TvServiceDto> list = new ArrayList<>();
+        list.add(createWestWorld());
+        list.add(createPoi());
+        return list;
+    }
 
+    public TvServiceDto createWestWorld() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2011, Calendar.OCTOBER, 22, 0 , 0, 0);
+        return new TvServiceDto(101, "West world", 2, cal.getTime());
+    }
+
+    public TvServiceDto createPoi() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(2011, Calendar.SEPTEMBER, 30, 0, 0, 0);
+        return new TvServiceDto(102, "Person of Intrest", 1, cal.getTime());
     }
 
 }
---
+++ SpringIoC/demo/src/main/java/top/jhana/demo/TvServiceDto.java
@@ -0,0 +1,54 @@
+package top.jhana.demo;
+
+import java.util.Date;
+
+public class TvServiceDto {
+
+    public TvServiceDto() {
+
+    }
+
+    public TvServiceDto(Integer id, String name, int seasonCount, Date originRelease) {
+        this.id = id;
+        this.name = name;
+        this.seasonCount = seasonCount;
+        this.originRelease = originRelease;
+    }
+
+    private Integer id;
+    private String name;
+    private int seasonCount;
+    private Date originRelease;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeasonCount() {
+        return seasonCount;
+    }
+
+    public void setSeasonCount(int seasonCount) {
+        this.seasonCount = seasonCount;
+    }
+
+    public Date getOriginRelease() {
+        return originRelease;
+    }
+
+    public void setOriginRelease(Date originRelease) {
+        this.originRelease = originRelease;
+    }
+}
