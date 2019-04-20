8f6dc7faddbcfcf340cd679d622b9bce48b09cbd
kksh3ll <kksh3ll@gmail.com>
2019-04-20 23:35:21 +0800

    thymeleaf for html

--- SpringIoC/thymeleaf/src/main/java/top/jhana/controller/UserController.java
+++ SpringIoC/thymeleaf/src/main/java/top/jhana/controller/UserController.java
@@ -1,9 +1,91 @@
 package top.jhana.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import top.jhana.domain.User;
+import top.jhana.repository.UserRepository;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
 
+    /**
+     * 获取用户列表
+     * @param model
+     * @return
+     */
+    @GetMapping
+    public ModelAndView list(Model model) {
+        model.addAttribute("userList", userRepository.userList());
+        model.addAttribute("title", "用户管理");
+        return new ModelAndView("users/list", "userModel", model);
+    }
+
+    /**
+     * 查看用户
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("{id}")
+    public ModelAndView view(@PathVariable("id") Long id, Model model) {
+
+        User user = userRepository.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("title", "查看用户");
+        return new ModelAndView("users/view", "userModel", model);
+    }
+
+    /**
+     * 创建表单
+     * @param model
+     * @return
+     */
+    @GetMapping("/form")
+    public ModelAndView createForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("title", "创建用户");
+        return new ModelAndView("users/form", "userModel", model);
+    }
+
+    /**
+     * 增加或更新用户
+     * @param user
+     * @return
+     */
+    @PostMapping
+    public ModelAndView saveOrUpdateUser(User user) {
+        userRepository.saveOrUpdateUser(user);
+        return new ModelAndView("redirect:/users");
+    }
+
+    /**
+     * 删除用户
+     * @param id
+     * @return
+     */
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteUser(@PathVariable("id") Long id) {
+        userRepository.deleteUser(id);
+        return new ModelAndView("Redirect:/users");
+    }
+
+    /**
+     * 获取修改用户的界面
+     * @param id
+     * @param model
+     * @return
+     */
+    @GetMapping("/modify/{id}")
+    public ModelAndView modify(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.getUserById(id);
+        model.addAttribute("user", user);
+        model.addAttribute("title", "修改用户");
+        return new ModelAndView("/users/form", "userModel", model);
+    }
 }
